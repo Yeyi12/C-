16ce071f9f96c5877b2fefd884c80712bc5e0efc
Yeyi12 <150805046+Yeyi12@users.noreply.github.com>
2023-12-20 20:51:46 +0000

    Update Hillclimber.py

--- Hillclimber.py
+++ Hillclimber.py
@@ -1,41 +1,39 @@
 import random
-import copy
-import matplotlib.pyplot as plt
-import numpy as np
-N = 20
-P = 100
-G = 200
-MUTRATE =0.008
-MUTSTEP =1.0
-MAX = 10
-MIN = -10
+import math
 
+MIN=-10
+MAX=10
+N=20
+LOOPS =500*200
 class solution:
     variable = [0]*N
     utility = 0
 individual = solution()
 
 for j in range (N):
-    individual.variable[j] = random.randint(0,100)
+    individual.variable[j] = random.uniform(MIN,MAX)
 individual.utility = 0
 
 def test_function( ind ):
+    utility = 0
     b=0
-    for i in range (1,N):   
-        b = b + (i*(2*ind.variable[i])**2 - (ind.variable[i-1]))**2
-    a = (ind.variable[0]-1)**2
+    for i in range (N):
+       b = b + ( ind.variable[i]**2 - (10* math.cos((2*math.pi)*ind.variable[i])))   
+    a = 10*N
     utility = a + b
     return utility
 
+
+individual.utility= test_function(individual)
 newind = solution()
-for x in range (P):
+for x in range (LOOPS):
     for i in range(N):
         newind.variable[i] = individual.variable[i]
     change_point = random.randint(0, N-1)
-    newind.variable[change_point] = random.randint(0,100)
+    newind.variable[change_point] = random.uniform(MIN,MAX)
     newind.utility = test_function( newind )
-    if individual.utility <= newind.utility:
+    if individual.utility >= newind.utility:
         individual.variable[change_point] = newind.variable[change_point]
         individual.utility = newind.utility
-
-plt.plot(newind)
+    print(individual.utility)
+    
